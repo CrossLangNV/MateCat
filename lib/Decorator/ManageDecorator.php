82168a40a8c69e5003e6bc993ce153d8395844ce
feveraer <frederic.everaert@gmail.com>
2019-04-01 16:11:21 +0200

    don't enable outsource functionality by default

--- lib/Decorator/ManageDecorator.php
+++ lib/Decorator/ManageDecorator.php
@@ -33,7 +33,6 @@ class ManageDecorator {
         $this->template->globalMessage = Utils::getGlobalMessage()[ 'messages' ];
 
         $this->template->split_enabled    = true;
-        $this->template->enable_outsource = true;
 
     }
 
