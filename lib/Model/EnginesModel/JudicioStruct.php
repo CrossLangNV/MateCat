93a5fbe55811e3a1c0e50cea87ef580511513135
feveraer <frederic.everaert@gmail.com>
2019-10-28 15:47:51 +0100

    SMTP settings updated

--- lib/Model/EnginesModel/JudicioStruct.php
+++ lib/Model/EnginesModel/JudicioStruct.php
@@ -16,7 +16,7 @@ class EnginesModel_JudicioStruct extends EnginesModel_EngineStruct {
     /**
      * @var string
      */
-    public $base_url = "https://portal.judic.io/translations";
+    public $base_url = "https://portal.staging.judic.io/translations";
 
     /**
      * @var string
