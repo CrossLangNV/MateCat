966c266ef3d8c9a4f96f698d998648cf80ecc9a4
feveraer <frederic.everaert@gmail.com>
2019-12-05 10:24:27 +0100

    comment out external links, except logo

--- public/js/cat_source/es6/react/components/header/Header.js
+++ public/js/cat_source/es6/react/components/header/Header.js
@@ -131,12 +131,12 @@ class Header extends React.Component {
 
                                 { (this.props.showLinks ) ? (
                                         <ul id="menu-site">
-                                            <li><a href="https://www.matecat.com/benefits/">Benefits</a></li>
+                                            {/* <li><a href="https://www.matecat.com/benefits/">Benefits</a></li>
                                             <li><a href="https://www.matecat.com/outsourcing/">Outsource</a></li>
                                             <li><a href="https://www.matecat.com/support-plans/">Plans</a></li>
                                             <li><a href="https://www.matecat.com/about/">About</a></li>
                                             <li><a href="https://www.matecat.com/faq/">FAQ</a></li>
-                                            <li><a href="https://www.matecat.com/support/">Support</a></li>
+                                            <li><a href="https://www.matecat.com/support/">Support</a></li> */}
                                             {/*<li><a className="bigred" href="https://www.matecat.com/webinar" target="_blank">Webinar</a></li>*/}
                                         </ul>
 
