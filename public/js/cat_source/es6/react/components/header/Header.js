28fdc7497deef475e154df74123b224616dd9794
anna.bardadym <anna.bardadym@crosslang.com>
2020-01-28 11:22:15 +0100

    admin module pre-color-picker

--- public/js/cat_source/es6/react/components/header/Header.js
+++ public/js/cat_source/es6/react/components/header/Header.js
@@ -72,7 +72,7 @@ class Header extends React.Component {
                         onClick={this.openPreferencesModal.bind(this)}>{config.userShortName}</div>
         } else {
             return <div className="ui user-nolog label" onClick={this.openLoginModal.bind(this)} title="Login">
-	                    <i className="icon-user22"/>
+                        <i className="icon-user22"/>
                     </div>
 
         }
@@ -110,7 +110,7 @@ class Header extends React.Component {
                     <nav className="sixteen wide column navigation">
                         <div className="ui grid">
                             <div className="six wide column">
-                                <a href="/" className="logo"/>
+                                <a href="/" className="logo"><img src="/public/img/logo.png"/></a>
                                 <a href="https://www.matecat.com/" className="logo-matecat"/>
                             </div>
 
@@ -131,12 +131,12 @@ class Header extends React.Component {
 
                                 { (this.props.showLinks ) ? (
                                         <ul id="menu-site">
-                                            {/* <li><a href="https://www.matecat.com/benefits/">Benefits</a></li>
+                                            <li><a href="https://www.matecat.com/benefits/">Benefits</a></li>
                                             <li><a href="https://www.matecat.com/outsourcing/">Outsource</a></li>
                                             <li><a href="https://www.matecat.com/support-plans/">Plans</a></li>
                                             <li><a href="https://www.matecat.com/about/">About</a></li>
                                             <li><a href="https://www.matecat.com/faq/">FAQ</a></li>
-                                            <li><a href="https://www.matecat.com/support/">Support</a></li> */}
+                                            <li><a href="https://www.matecat.com/support/">Support</a></li>
                                             {/*<li><a className="bigred" href="https://www.matecat.com/webinar" target="_blank">Webinar</a></li>*/}
                                         </ul>
 
